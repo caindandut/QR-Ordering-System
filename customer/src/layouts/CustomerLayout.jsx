2595b3afa1beb96bc845feff676d5eb94c256c5c
ledangkhanhdut <khanhdangabc2@gmail.com>
2025-11-11 21:16:19 +0700

    Tạo CustomerLayout.jsx để chứa Header và CartButton một cách cố định

---
+++ customer/src/layouts/CustomerLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import CustomerHeader from '../components/CustomerHeader';
+import CartButton from '../components/CartButton';
+
+export default function CustomerLayout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* 1. Header cố định */}
+      <CustomerHeader />
+      
+      {/* 2. "Lỗ hổng" cho các trang con (Menu, Giỏ hàng) */}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      
+      {/* 3. "Giỏ hàng mini" nằm trên cùng */}
+      <CartButton />
+    </div>
+  );
+}
